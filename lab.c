e07b51fc1a562c96a16911f6fc7e969f1dd2c6bf
Ura1438 <Ura_sup@mail.ru>
2025-04-24 12:16:08 +0300

    Update lab.c

--- lab.c
+++ lab.c
@@ -2,218 +2,330 @@
 
 void lab1()
 {
-printf("Задание: найти корни квадратного уравнения\n");
-float A, B, C, X1, X2;
-printf("Коэффициент A=");
-scanf("%f", &A);
-printf("Коэффициент B=");
-scanf("%f", &B);
-printf("Коэффициент C=");
-scanf("%f", &C);
-X1=(-B-sqrt(B*B-4*A*C))/(2*A);
-X2=(-B+sqrt(B*B-4*A*C))/(2*A);
-printf("X1=%f \n",X1);
-printf("X2=%f \n",X2);
+    printf("Задание: найти корни квадратного уравнения\n");
+    float A, B, C, X1, X2;
+    printf("Коэффициент A=");
+    scanf("%f", &A);
+    printf("Коэффициент B=");
+    scanf("%f", &B);
+    printf("Коэффициент C=");
+    scanf("%f", &C);
+    X1=(-B-sqrt(B*B-4*A*C))/(2*A);
+    X2=(-B+sqrt(B*B-4*A*C))/(2*A);
+    printf("X1=%f \n",X1);
+    printf("X2=%f \n",X2);
 }
 void lab1d()
 {
-printf("Задание: найти корни квадратного уравнения\n");
-printf("Создать 2 функции, считающие X1, X2 и вызвать их в main()\n");
-float x1(float A, float B, float C)
-{
-float X1=(-B-sqrt(B*B-4*A*C))/(2*A);
-return X1;
-}
+    printf("Задание: найти корни квадратного уравнения\n");
+    printf("Создать 2 функции, считающие X1, X2 и вызвать их в main()\n");
+    float x1(float A, float B, float C)
+    {
+        float X1=(-B-sqrt(B*B-4*A*C))/(2*A);
+        return X1;
+    }
 
-float x2(float A, float B, float C)
-{
-float X2=(-B+sqrt(B*B-4*A*C))/(2*A);
-return X2;
-}
+    float x2(float A, float B, float C)
+    {
+        float X2=(-B+sqrt(B*B-4*A*C))/(2*A);
+        return X2;
+    }
 
-float A, B, C, X1, X2;
-printf("Коэффициент A=");
-scanf("%f", &A);
-printf("Коэффициент B=");
-scanf("%f", &B);
-printf("Коэффициент C=");
-scanf("%f", &C);
-X1=x1(A,B,C);
-X2=x2(A,B,C);
-printf("X1=%f \n",X1);
-printf("X2=%f \n",X2);
+    float A, B, C, X1, X2;
+    printf("Коэффициент A=");
+    scanf("%f", &A);
+    printf("Коэффициент B=");
+    scanf("%f", &B);
+    printf("Коэффициент C=");
+    scanf("%f", &C);
+    X1=x1(A,B,C);
+    X2=x2(A,B,C);
+    printf("X1=%f \n",X1);
+    printf("X2=%f \n",X2);
 
 }
 void lab2()
 {
-printf("Вычислить сумму первых элементов ряда. Вычисления проводить пока модуль очередного элемента больше эпсилон\n");
-float s,a,b,c,eps;
-int i;
+    printf("Вычислить сумму первых элементов ряда. Вычисления проводить пока модуль очередного элемента больше эпсилон\n");
+    float s,a,b,c,eps;
+    int i;
 
-printf("eps=");
-scanf("%f",&eps);
+    printf("eps=");
+    scanf("%f",&eps);
 
-s=0;
-i=0;
-c=1;
-b=1;
-a=c/b;
+    s=0;
+    i=0;
+    c=1;
+    b=1;
+    a=c/b;
 
-while(a>eps)
-{
-s=s+a;
-c=c*(c+2);
-b=b*(b+3);
-a=c/b;
-i=i+1;
-}
+    while(a>eps)
+    {
+        s=s+a;
+        c=c*(c+2);
+        b=b*(b+3);
+        a=c/b;
+        i=i+1;
+    }
 
-printf("n=%d, s=%f\n", i,s);
+    printf("n=%d, s=%f\n", i,s);
 }
 void lab2d()
 {
-printf("Вычислить сумму первых элементов ряда. Вычисления проводить пока модуль очередного элемента больше эпсилон\n");
-printf("Выходить из цикла при сумме больше 3 при помощи break\n");
-float s,a,b,c,eps;
-int i;
+    printf("Вычислить сумму первых элементов ряда. Вычисления проводить пока модуль очередного элемента больше эпсилон\n");
+    printf("Выходить из цикла при сумме больше 3 при помощи break\n");
+    float s,a,b,c,eps;
+    int i;
 
-printf("eps=");
-scanf("%f",&eps);
+    printf("eps=");
+    scanf("%f",&eps);
 
-s=0;
-i=0;
-c=1;
-b=1;
-a=c/b;
+    s=0;
+    i=0;
+    c=1;
+    b=1;
+    a=c/b;
 
-while(a>eps)
-{
-s=s+a;
-c=c*(c+2);
-b=b*(b+3);
-a=c/b;
-i++;
-if(s>3)
-{
-break;
-}
-}
+    while(a>eps)
+    {
+        s=s+a;
+        c=c*(c+2);
+        b=b*(b+3);
+        a=c/b;
+        i++;
+        if(s>3)
+        {
+            break;
+        }
+    }
 
-printf("n=%d, s=%f\n", i,s);
+    printf("n=%d, s=%f\n", i,s);
 }
 void lab3()
 {
-printf("Определить, являются ли все слова из потока словами одинаковой длины\n");
-int c, cnt, x;
+    printf("Определить, являются ли все слова из потока словами одинаковой длины\n");
+    int c, cnt, x;
 
-cnt=0;
-x=0;
+    cnt=0;
+    x=0;
 
-while((c=getchar())!=EOF)
-{
-if(c==' '||c=='.'||c=='\n'||c ==',')
-{
-if(x!=cnt&&x!=0)
-{
-printf("No\n");
-break;
-}
-else
-{
-x=cnt;
-cnt=0;
-}
-}
-else
-{
-cnt=cnt+1;
-}
-}
-printf("Yes\n");
-}
-void lab4()
-{
-printf("Тема лабы");
-#define YES 1
-#define NO 0
-#define MAXLINE 1000
-
-void process_line(char buffer[]);
-
-int main()
-{
-    char line[MAXLINE];
-
-    fgets(line, MAXLINE, stdin);
-    process_line(line);
-    puts(line);
-    return 0;
-}
-
-void process_line(char buffer[]);
-{
-    char c, buffer[MAXLINE];
-    int prev_c, flag, found, i, pos, start, j;
-
-    flag=NO;
-    found=NO;
-    prev_c=' ';
-    start=0;
-    i=0;
-    pos=0;
-
-    do
+    while((c=getchar())!=EOF)
     {
-        c=buffer[i];
-        if(c==' '||c=='.'||c=='\n'||c==','||c=='\0')
+        if(c==' '||c=='.'||c=='\n'||c ==',')
         {
-            if(flag==YES)
+            if(x!=cnt&&x!=0)
             {
-                if(found==NO)
-                {
-                    for(j=start; j<i; j++)
-                        buffer[pos++]=buffer[j];
-                }
+                printf("No\n");
+                break;
             }
-            flag=NO;
-            buffer[pos++]=c;
+            else
+            {
+                x=cnt;
+                cnt=0;
+            }
         }
         else
         {
-            if(flag==NO)
-                start=i;
-            if(c>='0'&&c<='9')
-                found=YES;
-            else
-            {
-                found=NO;
-                flag=YES;
-            }
+            cnt=cnt+1;
+        }
+    }
+    printf("Yes\n");
+}
+void process_line(char buffer[]);
+void clear_input_buffer();
+void lab4()
+{
+    printf("В символьной строке удалить все числа\n");
+    char line[MAXLINE];
+    clear_input_buffer();
+    if(fgets(line, MAXLINE, stdin)) 
+    {
+        process_line(line);
+        printf("Результат: %s", line);
+    }
+}
+void process_line(char buffer[])
+{
+    int i = 0;
+    int pos = 0;
+    while(buffer[i]!='\0')
+    {
+        if(!(buffer[i]>='0' && buffer[i]<='9'))
+        {
+            buffer[pos++]=buffer[i];
         }
         i++;
-    } while(c!=EOF);
+    }
+    buffer[pos]='\0';
 }
+void clear_input_buffer()
+{
+    int c;
+    while((c=getchar())!='\n'&&c!=EOF){}
 }
 void lab4d()
 {
-printf("Тема лабы");
-printf("Задание");
+    printf("Тема лабы");
+    printf("Задание");
 }
 void lab5()
 {
-printf("Тема лабы");
+    printf("В массиве из 10 целых чисел сосчитать сумму четных и нечетных элементов. Обнулить элементы, чья сумма меньше.\n");
+    int x[N], sr_even, sr_odd, i;
+    sr_even=0;
+    sr_odd=0;
+    printf("Изначальный массив:\n");
+    for(i=0; i<N; i++)
+    {
+        x[i]=rand()%1000;
+        printf("%d\n",x[i]);
+        if(i%2==0)
+            sr_even=sr_even+x[i];
+        else
+            sr_odd=sr_odd+x[i];
+    }
+    if(sr_even>sr_odd)
+    {
+        for(i=1; i<N; i=i+2)
+        {
+            x[i]=0;
+        }
+    }
+    else
+    {
+        for(i=0; i<N; i=i+2)
+        {
+            x[i]=0;
+        }
+    }
+    printf("\nИтог:\n");
+    for (i=0; i<N; i++)
+        printf("%d \n",x[i]);
+        printf("\n");
 }
 void lab5d()
 {
-printf("Тема лабы");
-printf("Задание");
+    printf("В массиве из 10 целых чисел сосчитать сумму четных и нечетных элементов. Обнулить элементы, чья сумма меньше.\n");
+    printf("Создать сортировку\n");
+
+    int x[N];
+    int sr_even;
+    int sr_odd;
+    int i,k,m,pm;
+
+    sr_even = 0;
+    sr_odd = 0;
+    printf("Изначальный массив:\n");
+    for(i = 0; i < N; i++)
+    {
+        x[i] = rand() % 1000;
+        printf("%d\n", x[i]);
+    }
+    for (k = 0; k < N - 1; k++) {
+        for (m = 0; m < N - k - 1; m++) {
+            if (x[m] > x[m + 1]) {
+                pm = x[m];
+                x[m] = x[m + 1];
+                x[m + 1] = pm;
+            }
+        }
+    }
+    printf("\nОтсортированный:\n");
+    for(i = 0; i < N; i++)
+    {
+        printf("%d\n", x[i]);
+    }
+    for(i = 0; i < N; i++) {
+        if(i % 2 == 0)
+            sr_even += x[i];
+        else
+            sr_odd += x[i];
+    }
+    if(sr_even > sr_odd)
+    {
+        for(i = 1; i < N; i = i + 2)
+        {
+            x[i] = 0;
+        }
+    }
+    else
+    {
+        for(i = 0; i < N; i = i + 2)
+        {
+            x[i] = 0;
+        }
+    }
+    printf("\nИтог:\n");
+    for (i = 0; i < N; i++)
+        printf("%d \n", x[i]);
+    printf("\n");
 }
 void lab6()
 {
-printf("Тема лабы");
+    printf("В двумерном массиве обнулять строки, среднее арифметическое которых меньше среднего массива\n");
+    #define K 3
+    #define M 4
+
+    int x[K][M], j, i, pos, aver, stroka, sr_stroka;
+
+    for(i = 0; i < K; i++)
+    {
+        for(j = 0; j < M; j++)
+        {
+            x[i][j]=rand()%1000;
+        }
+    }
+    //Вывод исходного массива
+    printf("Исходный массив %dx%d:\n", K, M);
+    for(i = 0; i < K; i++)
+    {
+        for(j = 0; j < M; j++)
+        {
+            printf("%4d", x[i][j]);
+        }
+        printf("\n");
+    }
+    //Вычисление общего среднего массива
+    aver=0;
+    for(i = 0; i < K; i++)
+    {
+        for(j = 0; j < M; j++)
+        {
+            aver+=x[i][j];
+        }
+    }
+    aver=aver/(K*M);
+    //Вычисление среднего для строки
+    for(i = 0; i < K; i++)
+    {
+        stroka=0;
+        for(j = 0; j < M; j++)
+        {
+            stroka+=x[i][j];
+        }
+        sr_stroka=stroka/M;
+        //Обнуление строки, если её среднее меньше общего
+        if(sr_stroka < aver)
+        {
+            for(j = 0; j < M; j++)
+            {
+                x[i][j]=0;
+            }
+        }
+    }
+    //Вывод итогового массива
+    printf("\nИтоговый массив:\n");
+    for(i = 0; i < K; i++)
+    {
+        for(j = 0; j < M; j++)
+        {
+            printf("%4d", x[i][j]);
+        }
+        printf("\n");
+    }
 }
 void lab6d()
 {
-printf("Тема лабы");
-printf("Задание");
+    printf("Тема лабы");
+    printf("Задание");
 }
